ec1b97a2bf5b4e0693b12ba0a3d1410842e9646f
renzoMoraS <rmora@alumno.huergo.edu.ar>
2019-11-02 11:48:15 -0300

    ya no se

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ var cookieParser = require('cookie-parser');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
