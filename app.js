7f9ffc1a153b2cbf9ce3e1e9f2c60defdb4e7949
renzoMoraS <rmora@alumno.huergo.edu.ar>
2019-11-02 12:50:54 -0300

    ya no se

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/.*',function(req,res) {
+app.get('/*',function(req,res) {
   res.sendFile('index.html');
 })
 
