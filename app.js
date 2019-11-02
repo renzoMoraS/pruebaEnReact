20db9e3e5fbe57dff89e80c5c8a8ca6ec506e435
renzoMoraS <rmora@alumno.huergo.edu.ar>
2019-11-02 13:01:46 -0300

    ya no se

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*',function(req,res) {
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
 
 // catch 404 and forward to error handler
