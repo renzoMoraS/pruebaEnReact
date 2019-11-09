b1572ff6157733a436db908882e46dade23f6924
renzoMoraS <rmora@alumno.huergo.edu.ar>
2019-11-09 09:36:52 -0300

    pinky recreate

---
+++ public/precache-manifest.acd70109760d599a5cc909aa65d18a78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0269b5daa79bbd98c52d3e24f8bb3385",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58be1c7321b5cc0adccf",
+    "url": "/static/css/2.878e5231.chunk.css"
+  },
+  {
+    "revision": "2247132ed12d2418585b",
+    "url": "/static/css/main.85c98634.chunk.css"
+  },
+  {
+    "revision": "58be1c7321b5cc0adccf",
+    "url": "/static/js/2.f56a9f32.chunk.js"
+  },
+  {
+    "revision": "2247132ed12d2418585b",
+    "url": "/static/js/main.80402738.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "47b76eff2240232b4ad0ce0e9a3dbcda",
+    "url": "/static/media/logo.47b76eff.png"
+  }
+]);
