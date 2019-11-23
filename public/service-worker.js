409b90b096080c6501dec4dcc8fae3b402c013b7
renzoMoraS <rmora@alumno.huergo.edu.ar>
2019-11-23 13:45:42 -0300

    Arreglando calamidades de Eze

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8402f363c90b0d282d27f92eb29ab0a1.js"
+  "/precache-manifest.8adce62dd2a2b0b61d060707f61ddb57.js"
 );
 
 self.addEventListener('message', (event) => {
