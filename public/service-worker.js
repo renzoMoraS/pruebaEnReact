5b0cf4f5197dcae134a8b161ab0c00838ab17e63
renzoMoraS <rmora@alumno.huergo.edu.ar>
2019-11-27 01:17:38 -0300

    Mergeando lo del chino2

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.010cd542019106441f43321a634e3d6d.js"
+  "/precache-manifest.8b0ca3e6ff14b8b9cfc300d215156582.js"
 );
 
 self.addEventListener('message', (event) => {
